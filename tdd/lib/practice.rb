6c4f56807e5bea9be6b0442e0ff1d7c209565370
Rika <ruijiasun1323@gmail.com>
2018-08-29 14:21:37 -0700

    implement two_sum

--- tdd/lib/practice.rb
+++ tdd/lib/practice.rb
@@ -1,7 +1,28 @@
 class Array 
   
   def remove_dups
-    return self.uniq
+    seen = []
+    self.each do |el|
+      seen.push(el) unless seen.include?(el)
+    end
+    seen
   end
+  
+  def two_sum
+    result = []
+    i = 0
+    while i < self.length
+      j = i + 1
+      while j < self.length
+        result.push([i,j]) if self[i] + self[j] == 0
+        j += 1
+      end
+      i += 1
+    end
+    result
+  end
+end
+
+def my_transpose(arr)
   
 end
--- tdd/spec/practice_spec.rb
+++ tdd/spec/practice_spec.rb
@@ -20,5 +20,18 @@ require 'rspec'
     
   end
   
+  describe '#two_sum' do
+    it 'finds pairs of numbers' do
+      expect([-1,0,2,-2,1].two_sum).to eq([[0,4], [2, 3]])
+    end
+  end
+  
+  describe '#my_transpose' do
+    it 'converts between row and col orientation' do
+      expect(my_transpose([[0,1,2],[3,4,5],[6,7,8]])).to eq([[0,3,6],[1,4,7],[2,5,8]])
+    end
+    
+  end
+  
 
     
